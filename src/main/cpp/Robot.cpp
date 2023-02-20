312d6b3582d30fc61237468057991db1ca2b079c
Terrrm <9992188+Terrrm@users.noreply.github.com>
2023-02-20 17:16:52 -0600

    Testing

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+#include <math.h>
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/drive/DifferentialDrive.h>
@@ -54,8 +55,7 @@ class DualChannelAnalogEncoder {
     {
       int valueA = m_inputA.GetValue();
       int valueB = m_inputB.GetValue();
-      frc::SmartDashboard::PutNumber("encoderA",    valueA);
-      frc::SmartDashboard::PutNumber("encoderB",    valueB);
+
       if ( valueA       > m_minValidValue && valueA       < m_maxValidValue &&
            m_prevValueA > m_minValidValue && m_prevValueA < m_maxValidValue )
       {
@@ -105,6 +105,7 @@ class Robot : public frc::TimedRobot {
   int const kLinActACanId  = 8;
   int const kLinActBCanId  = 9;
   int const kLiftCanId     = 10;
+  int const kClawRotateCanId = 11;
 
   WPI_TalonSRX                 m_frontleftMotor { kFLMotorCanId };
   WPI_TalonSRX                 m_rearleftMotor  { kRLMotorCanId };
@@ -114,6 +115,7 @@ class Robot : public frc::TimedRobot {
   WPI_VictorSPX                m_LinActRight    { kLinActACanId };
   WPI_VictorSPX                m_LinActLeft     { kLinActBCanId };
   WPI_VictorSPX                m_Lift           { kLiftCanId    };
+  WPI_VictorSPX                m_ClawRotate     { kClawRotateCanId };
 
 
   // Pneumatics
@@ -122,12 +124,12 @@ class Robot : public frc::TimedRobot {
 
 
   // Digial I/O
-  frc::Encoder      m_leftencoder     { 0, 1 };
+  frc::Encoder      m_leftencoder     { 6, 7 };
   frc::Encoder      m_rightencoder    { 2, 3 };
   frc::DigitalInput m_bottomLimitLeft { 4 };
   frc::DigitalInput m_bottomLimitRight{ 5 };
-  frc::DigitalInput m_liftLimitTop    { 6 };
-  frc::DigitalInput m_liftLimitBot    { 7 };
+  frc::DigitalInput m_liftLimitTop    { 0 };
+  frc::DigitalInput m_liftLimitBot    { 9 };
 
 
   // Analog I/O
@@ -161,10 +163,16 @@ class Robot : public frc::TimedRobot {
   frc::PIDController m_balancePid{ kBalanceP, kBalanceI, kBalanceD };
 
 
+  double kClawRotateP{ 2.0e-4 };
+  double kClawRotateI{ 0.00 };
+  double kClawRotateD{ 0.00 };
+  frc::PIDController m_ClawRotatePid{ kClawRotateP, kClawRotateI, kClawRotateD };
+
+
+
+
   // Other
   frc::Timer m_autoTimer;
-
-
 
   const std::string    kAutoNameDefault = "Default";
   const std::string    kAutoDrive       = "Drive";
@@ -223,7 +231,7 @@ class Robot : public frc::TimedRobot {
 
   void RobotPeriodic() override {
     m_angleEncoder.Update();
-    //m_clawEncoder.Update();
+    m_clawEncoder.Update();
 
     frc::SmartDashboard::PutNumber("m_leftencoder",      m_leftencoder.Get());   
     frc::SmartDashboard::PutNumber("m_rightencoder",     m_rightencoder.Get());
@@ -231,8 +239,8 @@ class Robot : public frc::TimedRobot {
     frc::SmartDashboard::PutNumber("m_bottomLimitRight", m_bottomLimitRight.Get());
     frc::SmartDashboard::PutNumber("m_angleEncoder",     m_angleEncoder.GetValue());
     frc::SmartDashboard::PutNumber("m_clawEncoder",      m_clawEncoder.GetValue());
-
-
+    frc::SmartDashboard::PutNumber("m_liftLimitTop",     m_liftLimitTop.Get());
+    frc::SmartDashboard::PutNumber("m_liftLimitBot",     m_liftLimitBot.Get());
   }
 
 
@@ -243,6 +251,7 @@ class Robot : public frc::TimedRobot {
     m_rightencoder.Reset();
     m_balancePid.Reset();
     m_balancePid.SetSetpoint( 0.0 );
+    m_ClawRotatePid.Reset();
   }
 
 
@@ -306,19 +315,30 @@ class Robot : public frc::TimedRobot {
     m_rightencoder.Reset();
     m_balancePid.Reset();
     m_balancePid.SetSetpoint( 0.0 );
+    m_ClawRotatePid.Reset();
   }
 
 
 
   void TeleopPeriodic() override {
-    bool SelfBalanceEnable = m_stick.GetYButton();
-    bool ToggleClaw        = m_stick.GetXButtonPressed();
+    bool SelfBalanceEnable = false;//m_stick.GetYButton();
+    bool ToggleClaw        = false;//m_stick.GetXButtonPressed();
 
     bool LiftUp            = false;//m_stick.GetYButtonPressed();
     bool LiftDown          = false;//m_stick.GetXButtonPressed();
 
+    bool Lift2Up            = m_stick.GetYButtonPressed();
+    bool Lift2Down          = m_stick.GetXButtonPressed();
+
+    bool ClawOpen           = m_stick.GetAButtonPressed();
+    bool ClawClose          = m_stick.GetBButtonPressed();
+
     bool AngleUp           = m_stick.GetRightBumper();
     bool AngleDown         = m_stick.GetLeftBumper();
+
+    bool RotateClawCW      = false;//m_stick.GetAButton();
+    bool RotateClawCCW     = false;//m_stick.GetBButton();
+
 
 
     // ------------------------------------------------------------------------
@@ -338,7 +358,7 @@ class Robot : public frc::TimedRobot {
     // ------------------------------------------------------------------------
     //  CLAW CONTROL
     // ------------------------------------------------------------------------
-    if ( ToggleClaw )
+    /*if ( ToggleClaw )
     {
       if ( m_clawState == CLAW_STATE_OPEN )
       {
@@ -361,6 +381,62 @@ class Robot : public frc::TimedRobot {
     else
     {
       m_clawSolenoid.Set(frc::DoubleSolenoid::Value::kOff);
+    }*/
+
+    if ( ClawOpen )
+    {
+      m_clawSolenoid.Set(frc::DoubleSolenoid::Value::kForward);
+    }
+    else if ( ClawClose )
+    {
+      m_clawSolenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+    }
+    else
+    {
+      m_clawSolenoid.Set(frc::DoubleSolenoid::Value::kOff);
+    }
+
+    if ( Lift2Up )
+    {
+      m_liftSolenoid.Set(frc::DoubleSolenoid::Value::kForward);
+    }
+    else if ( Lift2Down )
+    {
+      m_liftSolenoid.Set(frc::DoubleSolenoid::Value::kReverse);
+    }
+    else
+    {
+      m_liftSolenoid.Set(frc::DoubleSolenoid::Value::kOff);
+    }
+
+    /*
+    if ( RotateClawCW )
+    {
+      m_ClawRotate.Set( 0.5 );
+    }
+    else if ( RotateClawCCW )
+    {
+      m_ClawRotate.Set( -0.5 );
+    }
+    else
+    {
+      m_ClawRotate.Set( 0.0 );
+    }*/
+
+    double CountsPerTurn = 13000.0;
+    double ClawAngle = atan( m_stick.GetRightY() / m_stick.GetRightX() ) / ( 2.0 * 3.14159 );
+    double mag = m_stick.GetRightY() * m_stick.GetRightY() + m_stick.GetRightX() * m_stick.GetRightX();
+
+    if ( mag > 0.2 )
+    {
+      m_ClawRotatePid.SetSetpoint( ClawAngle * CountsPerTurn );
+      double clawPower = m_ClawRotatePid.Calculate( m_clawEncoder.GetValue() );
+
+      m_ClawRotate.Set( clawPower );
+    }
+    else
+    {
+      m_ClawRotate.Set( 0.0 );
     }
 
 
@@ -386,17 +462,17 @@ class Robot : public frc::TimedRobot {
     }
     else if( AngleUp )
     {
-      if ( m_angleEncoder.GetValue() > -1200 )
+      if ( m_angleEncoder.GetValue() > -1000 )
       {
         linActRightValue = -1.0;
         linActLeftValue  = -1.0;
       }
     }
 
-    frc::SmartDashboard::PutNumber("AngleDown",      AngleDown);
-    frc::SmartDashboard::PutNumber("AngleUp",      AngleUp);
-    frc::SmartDashboard::PutNumber("linActRightValue",      linActRightValue);
-    frc::SmartDashboard::PutNumber("linActLeftValue",      linActLeftValue);
+    frc::SmartDashboard::PutNumber("AngleDown",        AngleDown);
+    frc::SmartDashboard::PutNumber("AngleUp",          AngleUp);
+    frc::SmartDashboard::PutNumber("linActRightValue", linActRightValue);
+    frc::SmartDashboard::PutNumber("linActLeftValue",  linActLeftValue);
 
 
     m_LinActRight.Set( linActRightValue );
